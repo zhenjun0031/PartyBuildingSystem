75a9de57d8b5b81d382de74ebac67cefdf1baa62
root <2934636895@qq.com>
2025-06-10 23:31:37 +0800

    查询全部进度具体进度

--- partyBuilding-activity/src/main/java/com/partyBuilding/activity/service/IAdminTaskService.java
+++ partyBuilding-activity/src/main/java/com/partyBuilding/activity/service/IAdminTaskService.java
@@ -4,8 +4,8 @@ import com.partyBuilding.activity.domain.vo.PageVo;
 import com.partyBuilding.common.core.page.PageDomain;
 
 public interface IAdminTaskService {
-
+    //  查询全部进度
     PageVo selectProgress(Integer pageNum, Integer pageSize);
-
+    //查询具体进度
     PageVo detail(Integer pageNum, Integer pageSize, Long studentId);
 }
